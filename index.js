0b899488db06a797646ab6750eeb3b5a5e839f48
Tursunpulat2002 <tursunpulat2002@gmail.com>
2023-05-12 23:46:20 -0400

    Added restart button with functionality.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import passagesArray from "./src/passages.js";
 import game from "./src/game_state.js";
 
+let intervalID = null;
+
 function Timer() {
     let start = Date.now();
-    setInterval(function () {
-        let delta = Date.now() - start; // milliseconds elapsed since start
-        let deltaInSec = Math.floor(delta / 1000); // in seconds
-        document.getElementById("timer").innerHTML = "0:" + (60 - deltaInSec);
+    let counter = parseInt(document.getElementById("timer").innerHTML);
+    intervalID = setInterval(function () {
+        if (game.gameStart) {
+            if (counter == 0) {
+                document.getElementById("timer").innerHTML = "0";
+                return;
+            } else {
+                let delta = Date.now() - start; // milliseconds elapsed since start
+                let deltaInSec = Math.floor(delta / 1000); // in seconds
+                document.getElementById("timer").innerHTML = 60 - deltaInSec;
+                counter -= 1;
+            }
+        }
     }, 1000);
 }
 
@@ -20,28 +31,50 @@ function renderWords() {
     for (let i = 0; i < game.words.length; i++) {
         const spanElement = document.createElement("span");
         spanElement.setAttribute("id", i);
+        spanElement.setAttribute("class", "fs-4");
         spanElement.innerHTML = game.words[i];
         document.getElementById("random-words").appendChild(spanElement);
     }
 }
 
+function derenderWords() {
+    for (let i = 0; i < game.words.length; i++) {
+        const item = document.getElementById(i);
+        item.remove();
+    }
+}
+
 function renderCorrectWord(wordID) {
     let correctWordElement = document.getElementById(wordID);
-    correctWordElement.setAttribute("class", "text-success");
+    correctWordElement.setAttribute("class", "text-success fs-4");
     game.correctWords += 1;
     game.currentWord += 1;
 }
 
 function renderErrorWord(wordID) {
     let errorWordElement = document.getElementById(wordID);
-    errorWordElement.setAttribute("class", "text-danger");
+    errorWordElement.setAttribute("class", "text-danger fs-4");
     game.errorWords += 1;
     game.currentWord += 1;
 }
 
 function renderCurrentWord(wordID) {
     let currentWordElement = document.getElementById(wordID);
-    currentWordElement.setAttribute("class", "bg-primary");
+    currentWordElement.setAttribute("class", "bg-primary fs-4");
+}
+
+function restart() {
+    game.gameStart = false;
+    game.correctWords = 0;
+    game.errorWords = 0;
+    game.totalWords = 0;
+    game.words = [];
+    game.currentWord = 0;
+    game.gameStart = false;
+    document.getElementById("timer").innerHTML = 60;
+    let inputElement = document.getElementById("main_input");
+    inputElement.value = "";
+    inputElement.focus();
 }
 
 function init() {
@@ -52,6 +85,9 @@ function init() {
 
 document.getElementById("main_input").oninput = function (event) {
     if (!game.gameStart) {
+        if (intervalID) {
+            clearInterval(intervalID);
+        }
         Timer();
         game.gameStart = true;
     }
@@ -69,8 +105,12 @@ document.getElementById("main_input").oninput = function (event) {
     }
 };
 
-window.onload = function () {
+document.getElementById("restart").onclick = function () {
+    derenderWords();
+    restart();
     init();
 };
 
-// window.addEventListener("load", Text());
+window.onload = function () {
+    init();
+};
