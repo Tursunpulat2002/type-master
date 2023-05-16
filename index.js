c83b20f1987e55e1def2c460c803245475958321
Tursunpulat2002 <tursunpulat2002@gmail.com>
2023-05-15 20:58:50 -0400

    Added popup with final stat info

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ let intervalID = null;
 function Timer() {
     let start = Date.now(); // get the current time
     let counter = parseInt(document.getElementById("timer").innerHTML); // get the timer from html
-    let prevTotalWords = 0;
     intervalID = setInterval(function () {
         // call and save setInterval
         if (game.gameStart) {
@@ -18,7 +17,9 @@ function Timer() {
                 document.getElementById("timer").innerHTML = "0";
                 document.getElementById("main_input").disabled = true;
                 document.getElementById("wpm").innerHTML = game.correctWords;
+                document.getElementById("wpm2").innerHTML = game.correctWords;
                 game.gameStart = false;
+                document.getElementById("root-modal").setAttribute("style", "visibility: visible");
             } else {
                 // decrement the timer
                 let delta = Date.now() - start; // milliseconds elapsed since start
@@ -30,6 +31,7 @@ function Timer() {
                 let _wpm = parseInt(game.correctWords / (deltaInSec / 60));
                 let wpm = ("000" + _wpm).slice(-3);
                 document.getElementById("wpm").innerHTML = wpm;
+                document.getElementById("wpm2").innerHTML = wpm;
             }
         } else {
             return;
@@ -117,6 +119,7 @@ function init() {
     renderWords();
     renderCurrentWord(game.currentWord);
     document.getElementById("random-words").addEventListener("mousewheel", mouseHandler, false);
+    document.getElementById("root-modal").setAttribute("style", "visibility: hidden");
 }
 
 // run when anything is input in the input field
@@ -151,6 +154,7 @@ document.getElementById("main_input").oninput = function (event) {
         // update accuracy stat data
         if (game.typedWords > 0) {
             document.getElementById("progress-bar-container").setAttribute("class", "progress bg-danger");
+            document.getElementById("progress-bar-container2").setAttribute("class", "progress bg-danger");
         }
         document.getElementById("total-words").innerHTML = game.typedWords + " words";
         document.getElementById("correct-words").innerHTML = game.correctWords;
@@ -158,6 +162,12 @@ document.getElementById("main_input").oninput = function (event) {
         let percentCorrect = parseInt((game.correctWords / game.typedWords) * 100) + "%";
         document.getElementById("percent-correct").innerHTML = percentCorrect;
         document.getElementById("progress-bar-percent").setAttribute("style", "width: " + percentCorrect);
+
+        document.getElementById("total-words2").innerHTML = game.typedWords;
+        document.getElementById("correct-words2").innerHTML = game.correctWords;
+        document.getElementById("error-words2").innerHTML = game.errorWords;
+        document.getElementById("percent-correct2").innerHTML = percentCorrect;
+        document.getElementById("progress-bar-percent2").setAttribute("style", "width: " + percentCorrect);
     }
 };
 
@@ -174,6 +184,28 @@ document.getElementById("restart").onclick = function () {
     document.getElementById("percent-correct").innerHTML = "0%";
     document.getElementById("progress-bar-percent").setAttribute("style", "width: 0%");
     document.getElementById("progress-bar-container").setAttribute("class", "progress");
+    document.getElementById("root-modal").setAttribute("style", "visibility: hidden");
+};
+
+document.getElementById("restart2").onclick = function () {
+    derenderWords(); // get rid of the old word bank
+    restart(); // reset values
+    init(); // initialize new game
+    document.getElementById("main_input").disabled = false;
+    document.getElementById("wpm").innerHTML = "000";
+    document.getElementById("total-words").innerHTML = "0 words";
+    document.getElementById("correct-words").innerHTML = "0";
+    document.getElementById("error-words").innerHTML = "0";
+    document.getElementById("percent-correct").innerHTML = "0%";
+    document.getElementById("progress-bar-percent").setAttribute("style", "width: 0%");
+    document.getElementById("progress-bar-container").setAttribute("class", "progress");
+
+    document.getElementById("root-modal").setAttribute("style", "visibility: hidden");
+    document.getElementById("total-words2").innerHTML = "0";
+    document.getElementById("correct-words2").innerHTML = "0";
+    document.getElementById("error-words2").innerHTML = "0";
+    document.getElementById("percent-correct2").innerHTML = "0%";
+    document.getElementById("progress-bar-percent2").setAttribute("style", "width: 0%");
 };
 
 // initalize new game when the website first loads
